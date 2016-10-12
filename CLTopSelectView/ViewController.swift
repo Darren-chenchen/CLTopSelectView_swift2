5efa0b7368145cd28459dd43d6ed0cacdfad9c73
Darren-chenchen <1597887620@qq.com>
2016-10-12 21:08:47 +0800

    初始化
    初始化

---
+++ CLTopSelectView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CLTopSelectView
+//
+//  Created by darren on 16/10/12.
+//  Copyright © 2016年 darren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+       let selectView = CLTopSelectView.selectViewShow(CGRect(x: 0, y: 20, width: self.view.frame.size.width, height: 40), TitleArray: ["热门","最新最新","销量最新","价格","最新热门"], defaltSelectedIndex: 0, scrollEnable: false, lineEqualWidth: true, btnColor:UIColor.black,btnSelectedColor:UIColor.red,btnFont:15,titleClick: { (index:NSInteger) in
+            print(index)
+        })
+//       selectView.indicatorViewHeight = 5   // 自定义指示器的高度
+//        selectView.selectedIndex = 5  // 指定选中哪个
+        self.view.addSubview(selectView)
+    }
+
+
+}
+
